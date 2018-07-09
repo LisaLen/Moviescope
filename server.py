0ceb3848de1bccb876015574dd2f398d0c739bd3
vagrant <vagrant@vagrant.vm>
2018-07-09 19:58:48 +0000

    Initial commit

---
+++ server.py
@@ -0,0 +1,34 @@
+from flask import (Flask, request, render_template, redirect, flash, session)
+
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+
+app.secret_key = 'ABC'
+
+@app.route('/')
+def open_singin_singup_page():
+    '''Show SingIn/SingUp page'''
+
+    print('opening signin page')
+
+    return render_template('singin_singup.html')
+
+@app.route('/homepage')
+def open_homepage():
+    ''' Show homepage'''
+
+    return render_template('homepage.html')
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    DebugToolbarExtension(app)
+    app.run(debug=True, port=5000, host="0.0.0.0")
